81fce1b50c839883e7cbdf883bc9b665d6b09468
yangxcc <1434890515@qq.com>
2022-09-30 22:41:14 +0800

    双指针解决数组问题

---
+++ 数组/remove_element/remove_ele.go
@@ -0,0 +1,23 @@
+package removeelement
+
+func removeElement(nums []int, val int) int {
+	// 双指针
+	left := 0
+	right := len(nums) - 1
+	// 当left == right + 1的时候跳出来
+	for left <= right {
+		if nums[left] != val {
+			left++
+		} else {
+			if nums[right] == val {
+				right--
+			} else {
+				// swap
+				tmp := nums[left]
+				nums[left] = nums[right]
+				nums[right] = tmp
+			}
+		}
+	}
+	return left
+}
