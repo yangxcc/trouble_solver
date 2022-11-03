706ef988ae833ecad3d4ab747a8b2bbd018040fe
yangxcc <1434890515@qq.com>
2022-11-03 17:12:34 +0800

    修剪bst

---
+++ tree/bst/trim.go
@@ -0,0 +1,22 @@
+package bst
+
+import "trouble_solver/tree"
+
+// 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。
+func trimBST(root *tree.TreeNode, low int, high int) *tree.TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	// 这种情况下只需要返回右子树
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	}
+	if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
+}
