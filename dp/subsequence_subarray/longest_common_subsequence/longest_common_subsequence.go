0c9f7adeb071464588df9d4e802b360e68ac06a4
yangxcc <1434890515@qq.com>
2022-12-02 14:03:51 +0800

    最长公共子序列的应用，两个字符串的删除操作

--- dp/subsequence_subarray/longest_common_subsequence/longest_common_subsequence.go
+++ dp/subsequence_subarray/longest_common_subsequence/longest_common_subsequence.go
@@ -31,3 +31,14 @@ func max(a, b int) int {
 	}
 	return a
 }
+
+/**
+leetcode 583 两个字符串的删除操作
+	给定两个单词word1和word2，返回使得word1和word2相同所需的最小步数。
+	每步可以删除任意一个字符串中的一个字符。
+*/
+// 思路就是求得两个字符串的最长公共子序列
+func minDistance(word1 string, word2 string) int {
+	lcs := longestCommonSubsequence(word1, word2)
+	return len(word1) - lcs + len(word2) - lcs
+}
