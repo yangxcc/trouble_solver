d7c054a835b6ec6ca6de5d37058626d3ae78e9e6
yangxcc <1434890515@qq.com>
2022-12-12 22:27:22 +0800

    差分数组的应用（Java）

---
+++ array/diff_array/application/FlightBooking.java
@@ -0,0 +1,14 @@
+/*
+ * @Author: yangxcc
+ * @version: 1.0
+ * @Date: 2022-12-12 21:30:30
+ * @LastEditTime: 2022-12-12 22:20:09
+ */
+package array.diff_array.application;
+
+public class FlightBooking {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        // 同理...
+        return new int[n];
+    }
+}
