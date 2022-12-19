08dfd7042556ce67f6c9c5bc95012de721d0a515
yangxcc <1434890515@qq.com>
2022-12-19 15:14:46 +0800

    优化归并排序的写法

--- sort/merge/merge_sort.go
+++ sort/merge/merge_sort.go
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-12-13 19:46:09
- * @LastEditTime: 2022-12-13 20:05:30
+ * @LastEditTime: 2022-12-19 15:13:34
  */
 package merge
 
@@ -56,5 +56,52 @@ func merge(nums, tmp []int, left, mid, right int) []int {
 		nums[k] = tmp[k]
 	}
 
+	return nums
+}
+
+func MergeSortBetter(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	tmp := make([]int, len(nums))
+
+	var merge func(nums []int, left, mid, right int)
+	merge = func(nums []int, left, mid, right int) {
+		// 先将[left, right]这个区间内的数值拷贝到tmp中，以便能够直接将结果写入到nums中
+		for k := left; k <= right; k++ {
+			tmp[k] = nums[k]
+		}
+
+		i, j := left, mid+1
+		for p := left; p <= right; p++ {
+			if i == mid+1 {
+				// 左边的已经全都合并完了
+				tmp[p] = nums[j]
+				j++
+			} else if j == right+1 {
+				tmp[p] = nums[i]
+				i++
+			} else if tmp[i] > tmp[j] {
+				tmp[p] = nums[j]
+				j++
+			} else {
+				tmp[p] = nums[i]
+				i++
+			}
+		}
+	}
+
+	var sortHelper func(nums []int, left, right int)
+	sortHelper = func(nums []int, left, right int) {
+		if left < right {
+			mid := left + (right-left)/2
+			sortHelper(nums, left, mid)
+			sortHelper(nums, mid+1, right)
+			merge(nums, left, mid, right)
+		}
+	}
+
+	sortHelper(nums, 0, len(nums)-1)
 	return nums
 }
