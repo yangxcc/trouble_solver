c6ae98a2f998952934429c0dbc0ea59a015cec0e
yangxcc <1434890515@qq.com>
2023-01-09 11:23:15 +0800

    反转链表

--- linkedlist/reverse_linkedlist/ReserveList.java
+++ linkedlist/reverse_linkedlist/ReserveList.java
@@ -2,13 +2,14 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-10-05 19:33:29
- * @LastEditTime: 2022-12-30 17:45:23
+ * @LastEditTime: 2023-01-09 11:12:22
  */
 package linkedlist.reverse_linkedlist;
 
 import linkedlist.ListNode;
 
 public class ReserveList {
+    // leetcode 206 simple 反转链表
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -33,5 +34,43 @@ public class ReserveList {
             cur = tmp;
         }
         return pre;
+    }
+
+    /**
+     * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+     * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     * 
+     * leetcode 92 middle 反转链表2
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseNFromBegin(head, right);
+        }
+
+        head.next = reverseBetween(head.next, left-1, right-1);
+        return head;
+    }
+
+    // 反转前n个节点，n从1开始
+    ListNode reverseNext = null;
+    public ListNode reverseNFromBegin(ListNode head, int n) {
+        if (n <= 1) {
+            reverseNext = head.next;
+            return head;
+        }
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode reverseN = reverseNFromBegin(head.next, n-1);
+        head.next.next = head;
+        // head.next = reverseN.next; // 不能这么写，因为在这里时已经翻转过来了，比如1-2-3-4-5-6,n=3,此时已经是321了，3后面不是4，而是2了
+        head.next = reverseNext;
+
+        return reverseN;
     }
 }
--- linkedlist/reverse_linkedlist/reverse_list.go
+++ linkedlist/reverse_linkedlist/reverse_list.go
@@ -1,19 +1,27 @@
+/*
+ * @Author: yangxcc
+ * @version: 1.0
+ * @Date: 2022-10-05 19:32:06
+ * @LastEditTime: 2023-01-09 11:22:47
+ */
 package reverselinkedlist
 
-import "trouble_solver/linkedlist"
+import (
+	. "trouble_solver/linkedlist"
+)
 
 /*
 	反转列表总共有两种方法：双指针和递归
 */
 
 // 双指针
-func reverseListWithDoublePointer(head *linkedlist.ListNode) *linkedlist.ListNode {
+func reverseListWithDoublePointer(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		// 没有或者只有一个节点
 		return nil
 	}
-	var pre *linkedlist.ListNode = nil
-	var cur *linkedlist.ListNode = head
+	var pre *ListNode = nil
+	var cur *ListNode = head
 	for cur != nil {
 		// 保存下当前节点后面的节点
 		next := cur.Next
@@ -26,7 +34,7 @@ func reverseListWithDoublePointer(head *linkedlist.ListNode) *linkedlist.ListNod
 }
 
 // 递归
-func reverseListWithRecursion(head *linkedlist.ListNode) *linkedlist.ListNode {
+func reverseListWithRecursion(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
@@ -35,4 +43,27 @@ func reverseListWithRecursion(head *linkedlist.ListNode) *linkedlist.ListNode {
 	head.Next.Next = head
 	head.Next = nil
 	return recursion
+}
+
+func reverseList2(head *ListNode, left int, right int) *ListNode {
+	reverseNext := &ListNode{}
+	var reverseNFromBegin func(head *ListNode, n int) *ListNode
+	reverseNFromBegin = func(head *ListNode, n int) *ListNode {
+		if n <= 1 {
+			reverseNext = head.Next
+			return head
+		}
+
+		reverseN := reverseNFromBegin(head.Next, n-1)
+		head.Next.Next = head
+		head.Next = reverseNext
+		return reverseN
+	}
+
+	if left == 1 {
+		return reverseNFromBegin(head, right)
+	}
+
+	head.Next = reverseList2(head.Next, left-1, right-1)
+	return head
 }
