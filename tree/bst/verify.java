24aeba5ec1acf0e7601b6d31d8dad4f54ec69dd2
yangxcc <1434890515@qq.com>
2023-01-31 22:26:52 +0800

    BST的验证

--- tree/bst/verify.java
+++ tree/bst/verify.java
@@ -1,5 +1,34 @@
+/*
+ * @Author: yangxcc
+ * @version: 1.0
+ * @Date: 2022-11-02 10:41:55
+ * @LastEditTime: 2023-01-31 22:24:48
+ */
 package tree.bst;
 
-public class verify {
-    
+import tree.TreeNode;
+
+/**
+ * leetcode 98 middle 验证二叉搜索树
+ */
+public class Verify {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, null, null);
+    }
+
+    public boolean isValidBSTHelper(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+
+        if (max != null && root.val >- max.val) {
+            return false;
+        }
+
+        return isValidBSTHelper(root.left, min, root) && isValidBSTHelper(root.right, root, max);
+    }
 }
