550c561f8f5fe3959247f96c83f41bbcb4a28af0
yangxcc <1434890515@qq.com>
2023-02-03 16:09:59 +0800

    最经典的岛屿问题，岛屿数量

---
+++ graph/islands/IslandNumbers.java
@@ -0,0 +1,39 @@
+package graph.islands;
+
+/**
+ * leetcode 200 middle 岛屿数量
+ * 
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ */
+public class IslandNumbers {
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int res = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
