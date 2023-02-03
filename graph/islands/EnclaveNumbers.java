61faa7d07871964660a2c81c78fafcb1f9df1a23
yangxcc <1434890515@qq.com>
2023-02-03 15:20:06 +0800

    岛屿问题，飞地数量（dfs） --> 增加题目描述

--- graph/islands/EnclaveNumbers.java
+++ graph/islands/EnclaveNumbers.java
@@ -1,5 +1,12 @@
 package graph.islands;
 
+/**
+ * leetcode 1020 middle 飞地的数量
+ * 
+ * 给你一个大小为 m x n 的二进制矩阵 grid ，其中 0 表示一个海洋单元格、1 表示一个陆地单元格。
+ * 一次 移动 是指从一个陆地单元格走到另一个相邻（上、下、左、右）的陆地单元格或跨过 grid 的边界。
+ * 返回网格中 无法 在任意次数的移动中离开网格边界的陆地单元格的数量。
+ */
 public class EnclaveNumbers {
     public int numEnclaves(int[][] grid) {
         int m = grid.length; 
