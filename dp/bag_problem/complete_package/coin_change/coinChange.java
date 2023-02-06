b5a33cfd53efc112cf715afc2046ac11e7fcc917
yangxcc <1434890515@qq.com>
2023-02-06 20:42:33 +0800

    零钱兑换

--- dp/bag_problem/complete_package/coin_change/coinChange.java
+++ dp/bag_problem/complete_package/coin_change/coinChange.java
@@ -1,5 +1,29 @@
 package bag_problem.complete_package.coin_change;
 
-public class coinChange {
-    
+import java.util.Arrays;
+
+/**
+ * leetcode 322 middle 零钱兑换
+ * 
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * 你可以认为每种硬币的数量是无限的。
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        
+        for (int coin : coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
 }
