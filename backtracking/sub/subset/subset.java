bf3f8d579eafe78597874514a5af8b08fbf5ca16
yangxcc <1434890515@qq.com>
2023-02-06 11:10:05 +0800

    子集问题（无重复元素，不可重复选择）

--- backtracking/sub/subset/subset.java
+++ backtracking/sub/subset/subset.java
@@ -1,5 +1,32 @@
 package backtracking.sub.subset;
 
-public class subset {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 子集和组合问题是一样的
+ * 
+ * leetcode 78 middle 子集
+ * 
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ */
+public class Subset {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+
+        return ans;
+    }
+
+    public void backtrack(int[] nums, int idx, List<Integer> path) {
+        ans.add(new ArrayList<>(path));
+        
+        for (int i = idx; i < nums.length; i++) {
+            path.add(nums[i]);
+
+            backtrack(nums, i + 1, path);
+
+            path.remove(path.size() - 1);
+        }
+    }
 }
