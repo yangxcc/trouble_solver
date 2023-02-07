9fc211f4e746c0da0a0e1bb2c3b7da40ec0156d3
yangxcc <1434890515@qq.com>
2023-02-07 11:19:10 +0800

    最长公共子序列

--- dp/subsequence_subarray/longest_common_subsequence/longestCommonSubsequence.java
+++ dp/subsequence_subarray/longest_common_subsequence/longestCommonSubsequence.java
@@ -1,5 +1,38 @@
+/*
+ * @Author: yangxcc
+ * @version: 1.0
+ * @Date: 2022-12-02 12:30:18
+ * @LastEditTime: 2023-02-07 10:47:58
+ */
 package subsequence_subarray.longest_common_subsequence;
 
-public class longestCommonSubsequence {
-    
+/**
+ * leetcode 1143 middle 最长公共子序列
+ * 
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * 一个字符串的 子序列 是指这样一个新的字符串：
+ * 它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ */
+public class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        // dp[i][j]表示的是text1[0..i]和text2[0..j]的公共子序列长度
+        int[][] dp = new int[m + 1][n + 1];
+        // dp[0][i] = 0, dp[i][0] = 0
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 }
