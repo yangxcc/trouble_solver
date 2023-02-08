aadef12f24e5bf8cb25f3e0e41d80ce2b7a6b8e4
yangxcc <1434890515@qq.com>
2023-02-08 11:15:09 +0800

    不同路径（相较于1多了障碍物）

--- dp/path/path2.java
+++ dp/path/path2.java
@@ -1,5 +1,45 @@
 package path;
 
-public class path2 {
-    
+/**
+ * leetcode 63 middle 不同路径2
+ * 在 leetcode64（path1）的基础上在网格中加入了障碍物
+ */
+public class Path2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        if (obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+        // dp[i][j]表示的是从起点到[i, j]的路径数
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            // 第一列
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            // 第一行
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            } else {
+                dp[0][j] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j - 1] == 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+                if (obstacleGrid[i - 1][j] == 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
 }
