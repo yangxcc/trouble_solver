81aca4593193577706f26881105eb0c5c7534179
yangxcc <1434890515@qq.com>
2023-03-01 14:27:55 +0800

    对链表partition，注意断开原链表中的指针

--- linkedlist/split_linkedlist/split_linkedlist.go
+++ linkedlist/split_linkedlist/split_linkedlist.go
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-12-30 11:33:14
- * @LastEditTime: 2022-12-30 15:58:32
+ * @LastEditTime: 2023-03-01 14:26:20
  */
 package splitlinkedlist
 
@@ -14,6 +14,7 @@ import (
 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
 你应当 保留 两个分区中每个节点的初始相对位置。
 */
+// 对链表进行partition，先将链表拆成两个（小于的部分和大于等于的部分），然后再将两个链表合并
 func partition(head *ListNode, x int) *ListNode {
 	if head == nil {
 		return nil
