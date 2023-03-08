bbfe22b0356eeb7de1af8ece87c929e39f7c6731
yangxcc <1434890515@qq.com>
2023-03-08 22:40:31 +0800

    长度最小的子数组

--- array/slide_window/MinimumSizeSubarraySum.java
+++ array/slide_window/MinimumSizeSubarraySum.java
@@ -2,11 +2,13 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-10-01 12:07:32
- * @LastEditTime: 2023-01-02 17:16:21
+ * @LastEditTime: 2023-03-08 22:34:17
  */
 package array.slide_window;
 
 /**
+ * leetcode 209 simple 长度最小的子数组
+ * 
  * 给定一个含有 n 个正整数的数组和一个正整数 target 。
  * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。
  * 如果不存在符合条件的子数组，返回 0 。
