af01b5c2d90eb0ada00e90520ddf80b6ff0fd0ee
yangxcc <1434890515@qq.com>
2023-03-08 22:39:58 +0800

    无重复字符的最长子串

--- array/slide_window/LongestSubstring.java
+++ array/slide_window/LongestSubstring.java
@@ -2,12 +2,17 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2023-01-02 10:59:48
- * @LastEditTime: 2023-01-02 11:12:16
+ * @LastEditTime: 2023-03-08 22:27:37
  */
 package array.slide_window;
 
 import java.util.HashMap;
 
+/**
+ * leetcode 3 middle 无重复字符的最长子串
+ * 
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
 public class LongestSubstring{
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> window = new HashMap<>();
@@ -23,6 +28,16 @@ public class LongestSubstring{
                 window.put(l, window.get(l)-1);
                 left++;
             }
+
+            // 两种更新窗口的写法都对
+            // while (window.size() < right - left) {
+            //     char l = s.charAt(left);
+            //     window.put(l, window.get(l)-1);
+            //     if (window.get(l) == 0) {
+            //         window.remove(l);
+            //     }
+            //     left++;
+            // }
 
             maxLen = Math.max(maxLen, right-left);
         }
