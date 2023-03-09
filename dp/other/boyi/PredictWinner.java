3cbdde9a369a694ee1eaaa05007e46cc874670fe
yangxcc <1434890515@qq.com>
2023-03-09 16:11:06 +0800

    预测先手是否能赢（dp[i][j][0]表示在[i,j]区域内先手能够获得的最大收益）

--- dp/other/boyi/PredictWinner.java
+++ dp/other/boyi/PredictWinner.java
@@ -2,9 +2,11 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2023-02-07 21:32:15
- * @LastEditTime: 2023-02-08 09:39:35
+ * @LastEditTime: 2023-03-09 15:42:40
  */
 package other.boyi;
+
+import java.util.Scanner;
 
 /**
  * leetcode 486 middle 预测赢家
@@ -71,5 +73,48 @@ public class PredictWinner {
         }
 
         return dp[0][n - 1][0] >= dp[0][n - 1][1];
+    }
+}
+
+/**
+ * leetcode 486 middle 预测赢家
+ * 
+ * 给你一个整数数组 nums 。玩家 1 和玩家 2 基于这个数组设计了一个游戏。
+ * 玩家 1 和玩家 2 轮流进行自己的回合，玩家 1 先手。开始时，两个玩家的初始分值都是 0 。
+ * 每一回合，玩家从数组的任意一端取一个数字（即，nums[0] 或 nums[nums.length - 1]），
+ * 取到的数字将会从数组中移除（数组长度减 1 ）。玩家选中的数字将会加到他的得分上。当数组中没有剩余数字可取时，游戏结束。
+ * 
+ * 如果玩家 1 能成为赢家，返回 true 。如果两个玩家得分相等，同样认为玩家 1 是游戏的赢家，也返回 true 。
+ * 你可以假设每个玩家的玩法都会使他的分数最大化。
+ */
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(first(arr, 0, n - 1) > second(arr, 0, n - 1));
+    }
+
+    private static int first(int[] nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        } else if (start > end) {
+            return 0;
+        } else {
+            return Math.max(nums[start] + second(nums, start + 1, end), nums[end] + second(nums, start, end - 1));
+        }
+    }
+
+    private static int second(int[] nums, int start, int end) {
+        if (start >= end) {
+            return 0;
+        }
+
+        // 第二手选的目的就是让first下一次选的时候的结果是最小的
+        return Math.min(first(nums, start + 1, end), first(nums, start, end - 1));
     }
 }
