870c771355424e0acdddaf2502b9f96436994dc3
yangxcc <1434890515@qq.com>
2023-03-09 09:50:55 +0800

    电话号码的字母组合（ACM模式）

--- backtracking/combination/LetterCombinationOfPhoneNumber.java
+++ backtracking/combination/LetterCombinationOfPhoneNumber.java
@@ -2,12 +2,13 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-11-06 19:55:56
- * @LastEditTime: 2023-02-06 11:05:26
+ * @LastEditTime: 2023-03-09 09:49:50
  */
 package backtracking.combination;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * leetcode 17 middle 电话号码的字母组合
@@ -52,6 +53,55 @@ public class LetterCombinationOfPhoneNumber {
 
             // 在Java中这样不行，具体原因未知，大概是因为引用变了，前后两个path不一样了？
             // path = new StringBuilder(path.substring(0, path.length() - 1));
+            path.deleteCharAt(idx);
+        }
+    }
+}
+
+/**
+ * leetcode 17 middle 电话号码的字母组合
+ * 
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ */
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        backtrack(input, 0, new StringBuilder());
+
+        for (String path : ans) {
+            System.out.println(path);
+        }
+    }
+
+    static List<String> ans = new ArrayList<>();
+    static String[] record = new String[] {
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+    };
+
+    public static void backtrack(String input, int idx, StringBuilder path) {
+        if (path.length() == input.length()) {
+            ans.add(path.toString());
+            return;
+        }
+
+        String chs = record[input.charAt(idx) - '0'];
+        for (int i = 0; i < chs.length(); i++) {
+            path.append(chs.charAt(i));
+
+            backtrack(input, idx + 1, path);
+
             path.deleteCharAt(idx);
         }
     }
