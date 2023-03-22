7d2f1addd528cc69452e7b79207c129ee744ec6f
yangxcc <1434890515@qq.com>
2023-03-22 10:00:35 +0800

    新增 整型数 转ipv4地址的写法

--- skill/TranvseIPv4ToLongInt.java
+++ skill/TranvseIPv4ToLongInt.java
@@ -13,10 +13,15 @@ public class TranvseIPv4ToLongInt {
         String input = in.nextLine();
         in.close();
 
-        System.out.println(process1(input));
+        System.out.println(string2ip(input));
     }
 
-    public static long process1(String input) {
+    /**
+     * 
+     * 整数值 = (第一个整数 × 256³) + (第二个整数 × 256²) + (第三个整数 × 256) + 第四个整数
+     * 
+     */
+    public static long string2ip(String input) {
         // 因为.是转义字符，所以需要使用\\. 不能使用.
         String[] strs = input.split("\\.");
         System.out.println(Arrays.toString(strs));
@@ -28,5 +33,29 @@ public class TranvseIPv4ToLongInt {
         }
 
         return sum;
+    }
+
+    /**
+     * 
+     * 第一个整数 = 整数值 / 256³
+     * 第二个整数 = (整数值 % 256³) / 256²
+     * 第三个整数 = ((整数值 % 256³) % 256²) / 256
+     * 第四个整数 = ((整数值 % 256³) % 256²) % 256
+     * 
+     */
+    public static String ip2string(long ipAddress) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 3; i >= 0; i--) {
+            long power = (long) Math.pow(256, i);
+            int ip = (int) (ipAddress / power);
+            ipAddress = ipAddress % power;
+
+            sb.append(ip);
+            if (i != 3) {
+                sb.append(".");
+            }
+        }
+
+        return sb.toString();
     }
 }
