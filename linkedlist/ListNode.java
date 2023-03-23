c33cc2ce4856615e9a2ff9874aabcffc6bfae087
yangxcc <1434890515@qq.com>
2023-03-23 12:56:35 +0800

    添加随机指针

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -9,9 +9,12 @@ package linkedlist;
 public class ListNode {
     public int val;
     public ListNode next;
+    public ListNode random;
+
     public ListNode(int x) {
         this.val = x;
         this.next = null;
+        this.random = null;
     }
 }
 
