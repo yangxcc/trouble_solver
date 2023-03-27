602edddd4deef8f81a2806fd8a6cf1c5eaf5df61
yangxcc <1434890515@qq.com>
2023-03-27 21:08:11 +0800

    双向链表节点

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -10,11 +10,13 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode random;
+    public ListNode pre;
 
     public ListNode(int x) {
         this.val = x;
         this.next = null;
         this.random = null;
+        this.pre = null;
     }
 }
 
