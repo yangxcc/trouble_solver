c840139c926eca9384e8bc2079a32fe270feacbd
yangxcc <1434890515@qq.com>
2023-04-03 13:27:59 +0800

    合并两个排序的链表（递归和循环）

--- linkedlist/merge_linkedlist/MergeTwo.java
+++ linkedlist/merge_linkedlist/MergeTwo.java
@@ -2,12 +2,15 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-12-30 10:22:02
- * @LastEditTime: 2022-12-30 10:35:55
+ * @LastEditTime: 2023-04-03 10:51:17
  */
 package linkedlist.merge_linkedlist;
 
 import linkedlist.ListNode;
 
+/**
+ * leetcode 21/剑指offer25 simple 合并两个排序的链表
+ */
 public class MergeTwo {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) {
@@ -24,5 +27,29 @@ public class MergeTwo {
             list2.next = mergeTwoLists(list1, list2.next);
             return list2;
         }
+    }
+
+
+    private ListNode mergeTwoListsWithLoop(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                cur.next = list2;
+                break;
+            } else if (list2 == null) {
+                cur.next = list1;
+                break;
+            } else if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+                cur = cur.next;
+            } else if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
     }
 }
