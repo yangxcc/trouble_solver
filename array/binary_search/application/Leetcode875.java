9577fa28343971233b58cfe05097f7a82cc9446b
yangxcc <1434890515@qq.com>
2023-04-12 21:45:09 +0800

    珂珂爱吃香蕉，是用二分法解决应用题重点还是得确定好了最大值和最小值，findX的写法也得慎重

--- array/binary_search/application/Leetcode875.java
+++ array/binary_search/application/Leetcode875.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2023-01-06 21:26:30
- * @LastEditTime: 2023-01-06 21:54:18
+ * @LastEditTime: 2023-04-12 21:43:57
  */
 package array.binary_search.application;
 
@@ -11,7 +11,7 @@ package array.binary_search.application;
  * 
  * 珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
  * 珂珂可以决定她吃香蕉的速度 k （单位：根/小时）。
- * 每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。  
+ * 每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
  * 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
  * 返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
  * 
@@ -49,20 +49,69 @@ public class Leetcode875 {
                 h++;
             }
             // if (p <= eatSpeed) {
-            //     h++;
+            // h++;
             // } else {
-            //     while (p > 0) {
-            //         p -= eatSpeed;
-            //         h++;
-            //     }
+            // while (p > 0) {
+            // p -= eatSpeed;
+            // h++;
+            // }
             // }
         }
         return h;
     }
 
     public static void main(String[] args) {
-        int[] piles = new int[]{805306368,805306368,805306368};
+        int[] piles = new int[] { 805306368, 805306368, 805306368 };
         int x = minEatingSpeed(piles, 1000000000);
         System.out.println(x);
+    }
+}
+
+/**
+ * 珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
+ * 珂珂可以决定她吃香蕉的速度 k （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 k 根。
+ * 如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+ * 
+ * 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+ * 返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
+ */
+class day0412 {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minEatSpeed = 1, maxEatSpeed = 0;
+        for (int pile : piles) {
+            maxEatSpeed = Math.max(maxEatSpeed, pile);
+        }
+
+        while (minEatSpeed <= maxEatSpeed) {
+            int mid = minEatSpeed + (maxEatSpeed - minEatSpeed) / 2;
+            if (findH(piles, mid) == h) {
+                maxEatSpeed = mid - 1;
+            } else if (findH(piles, mid) < h) {
+                maxEatSpeed = mid - 1;
+            } else {
+                minEatSpeed = mid + 1;
+            }
+        }
+
+        return minEatSpeed;
+
+    }
+
+    /** 以speed的速度吃完piles需要多少小时 */
+    private int findH(int[] piles, int speed) {
+        int ans = 0;
+
+        for (int p : piles) {
+            if (p <= speed) {
+                ans++;
+            } else {
+                while (p > 0) {
+                    ans++;
+                    p -= speed;
+                }
+            }
+        }
+
+        return ans;
     }
 }
