73e2f40853555807aeb92bcbcc2a58020f6e959c
yangxcc <1434890515@qq.com>
2023-04-15 14:00:49 +0800

    差分数组的时候，根据题意少了diff数组的初始化，直接初始成0就好了

--- array/diff_array/application/FlightBooking.java
+++ array/diff_array/application/FlightBooking.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-12-12 21:30:30
- * @LastEditTime: 2023-02-23 16:27:59
+ * @LastEditTime: 2023-04-15 13:44:17
  */
 package array.diff_array.application;
 
@@ -52,5 +52,47 @@ class Diff {
             ans[i] = diff[i] + ans[i - 1];
         }
         return ans;
+    }
+}
+
+/**
+ * 这里有 n 个航班，它们分别从 1 到 n 进行编号。
+ * 有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 
+ * 意味着在从 firsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
+ * 请你返回一个长度为 n 的数组 answer，里面的元素是每个航班预定的座位总数。
+ */
+class day0415 {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        Difference diff = new Difference(n);
+        for (int[] book : bookings) {
+            diff.update(book[0], book[1], book[2]);
+        }
+
+        return diff.recover();
+    }
+
+    class Difference {
+        int[] diff;
+        
+        public Difference(int n) {
+            this.diff = new int[n];
+        }
+
+        public void update(int left, int right, int val) {
+            diff[left] += val;
+            if (right + 1 < diff.length) {
+                diff[right + 1] -= val;
+            }
+        }
+
+        public int[] recover() {
+            int[] ans = new int[diff.length];
+            ans[0] = diff[0];
+            for (int i = 1; i < diff.length; i++) {
+                ans[i] = ans[i - 1] + diff[i];
+            }
+
+            return ans;
+        }
     }
 }
