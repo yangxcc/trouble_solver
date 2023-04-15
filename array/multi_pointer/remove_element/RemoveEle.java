075fc9dfca8e1be9a30e568a0a0a81628407d890
yangxcc <1434890515@qq.com>
2023-04-15 17:24:24 +0800

    快慢指针解决一系列的问题：删除数组中的元素，删除数组中的重复元素

--- array/multi_pointer/remove_element/RemoveEle.java
+++ array/multi_pointer/remove_element/RemoveEle.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-09-30 21:32:23
- * @LastEditTime: 2022-12-31 20:33:28
+ * @LastEditTime: 2023-04-15 17:16:19
  */
 package array.multi_pointer.remove_element;
 
@@ -50,5 +50,41 @@ public class RemoveEle {
             fast++;
         }
         return slow+1;
+    }
+}
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 其实和移动零这道题是一模一样的，移动零的本质其实可以是删除数组中val等于0的元素
+ */
+class day0415 {
+    public int removeElement(int[] nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+
+    // leetcode 26 数组升序排列，让每个元素都只出现一次
+    public int removeDuplicates(int[] nums) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                nums[slow + 1] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow + 1;
     }
 }
