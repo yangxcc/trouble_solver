260aba0f25b051b5ed1ba2c2f9a95cffb9d0ba15
yangxcc <1434890515@qq.com>
2023-04-16 12:55:55 +0800

    满足条件的长度最小的子数组

--- array/slide_window/MinimumSizeSubarraySum.java
+++ array/slide_window/MinimumSizeSubarraySum.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-10-01 12:07:32
- * @LastEditTime: 2023-03-08 22:34:17
+ * @LastEditTime: 2023-04-16 12:55:33
  */
 package array.slide_window;
 
@@ -29,6 +29,38 @@ public class MinimumSizeSubarraySum{
                 int l = nums[left];
                 left++;
                 sumWindow -= l;
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
+
+
+/**
+ * leetcode 209 simple 长度最小的子数组
+ * 
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。
+ * 如果不存在符合条件的子数组，返回 0 。
+ */
+class day0416 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0, right = 0;
+
+        while (right < nums.length) {
+            int num = nums[right];
+            right++;
+
+            sum += num;
+            while (sum >= target) {
+                ans = Math.min(ans, right - left);
+                int n = nums[left];
+                left++;
+
+                sum -= n;
             }
         }
 
