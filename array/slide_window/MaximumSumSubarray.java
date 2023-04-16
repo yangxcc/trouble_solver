2e46895852daeac52b5dbd0549fc6cfeee151efe
yangxcc <1434890515@qq.com>
2023-04-16 10:54:25 +0800

    长度为 K 子数组中的最大和，需要满足两个条件，子数组长度为k && 窗口中无重复元素

--- array/slide_window/MaximumSumSubarray.java
+++ array/slide_window/MaximumSumSubarray.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-12-11 10:11:21
- * @LastEditTime: 2023-02-23 13:15:25
+ * @LastEditTime: 2023-04-16 10:53:05
  */
 package array.slide_window;
 
@@ -45,6 +45,51 @@ public class MaximumSumSubarray {
 
                 if (window.size() == k) {
                     ans = Math.max(ans, sumWindow);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k 。请你从 nums 中满足下述条件的全部子数组中找出最大子数组和：
+ * 子数组的长度是 k，且
+ * 子数组中的所有元素 各不相同 。
+ * 返回满足题面要求的最大子数组和。如果不存在子数组满足这些条件，返回 0 。
+ * 子数组 是数组中一段连续非空的元素序列。
+ */
+class day0416 {
+    public long maximumSubarraySum(int[] nums, int k) {
+        long ans = 0;
+        int left = 0, right = 0;
+        HashMap<Integer, Integer> window = new HashMap<>();
+        long sum = 0;
+
+        while (right < nums.length) {
+            int num = nums[right];
+            right++;
+
+            sum += num;
+            window.put(num, window.getOrDefault(num, 0) + 1);
+
+            if (right - left == k) {
+                if (window.size() == k) {
+                    ans = Math.max(ans, sum);
+                }
+            } else if (right - left > k) {
+                int n = nums[left];
+                left++;
+
+                sum -= n;
+                window.put(n, window.get(n) - 1);
+                if (window.get(n) == 0) {
+                    window.remove(n);
+                }
+
+                if (window.size() == k) {
+                    ans = Math.max(ans, sum);
                 }
             }
         }
