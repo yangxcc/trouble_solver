4b524655be0eec795cbcd3b5825741449129f60e
yangxcc <1434890515@qq.com>
2023-05-08 19:57:23 +0800

    求和（bugfix：增加dp数组的初始化）

--- hydro/Problem4.java
+++ hydro/Problem4.java
@@ -36,6 +36,9 @@ public class Problem4 {
 
         int target = sum / 2;
         int[][] dp = new int[n + 1][target + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
                 if (arr[i - 1] > j) {
