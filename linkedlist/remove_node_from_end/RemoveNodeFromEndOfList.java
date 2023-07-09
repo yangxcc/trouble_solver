e56ed5b6e52554054943ea8c566f5202c8870ab4
yangxcc <1434890515@qq.com>
2023-07-09 21:20:17 +0800

    删除倒数第n个节点（dummy）

--- linkedlist/remove_node_from_end/RemoveNodeFromEndOfList.java
+++ linkedlist/remove_node_from_end/RemoveNodeFromEndOfList.java
@@ -2,7 +2,7 @@
  * @Author: yangxcc
  * @version: 1.0
  * @Date: 2022-10-07 10:07:22
- * @LastEditTime: 2022-12-30 10:19:20
+ * @LastEditTime: 2023-07-09 21:19:54
  */
 package linkedlist.remove_node_from_end;
 
@@ -13,6 +13,7 @@ import linkedlist.ListNode;
  */
 public class RemoveNodeFromEndOfList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 注意要有dummy，这样的话删除第一个节点时，就不需要特殊逻辑了
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
         ListNode fast = dummy, slow = dummy;
