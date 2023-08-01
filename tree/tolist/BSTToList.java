5d5d495d854de813a3a50212efb1210f9ac5c886
yangxcc <1434890515@qq.com>
2023-08-01 16:45:23 +0800

    BST转双向链表

--- tree/tolist/BSTToList.java
+++ tree/tolist/BSTToList.java
@@ -1,2 +1,40 @@
-package tree.tolist;public class BSTToList {
+package tree.tolist;
+
+import tree.TreeNode;
+
+/**
+ * 剑指offer36 二叉搜索树与双向链表 middle https://leetcode.cn/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/
+ */
+public class BSTToList {
+    public TreeNode treeToDoublyList(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        inOrder(root);
+        // 连接起头尾
+        head.left = pre;
+        pre.right = head;
+
+        return head;
+    }
+
+    TreeNode pre = null;
+    TreeNode head = null; // 记录头节点的位置
+    private void inOrder(TreeNode cur) {
+        if (cur == null) {
+            return;
+        }
+
+        inOrder(cur.left);
+        if (pre == null) {
+            // 说明走到最左边了，记录下这个最左边的值（最小的值）
+            head = cur;
+        } else {
+            pre.right = cur;
+        }
+        cur.left = pre;
+        pre = cur;
+
+        inOrder(cur.right);
+    }
 }
