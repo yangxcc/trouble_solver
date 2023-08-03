c29c3e5d8b3101f6a87f82d4cedea1608eb50af8
yangxcc <1434890515@qq.com>
2023-08-03 09:26:43 +0800

    缺失的第一个正数

---
+++ skill/FirstMissingPosition.java
@@ -0,0 +1,39 @@
+package skill;
+
+/**
+ * leetcode 41 hard 缺失的第一个正整数
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ *
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ *
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ */
+public class FirstMissingPosition {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
