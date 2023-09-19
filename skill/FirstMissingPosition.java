a19b2a316caaef470e02d81cc8bd10e1a6095da0
yangxcc <1434890515@qq.com>
2023-09-19 20:55:45 +0800

    寻找数组中的重复数

--- skill/FirstMissingPosition.java
+++ skill/FirstMissingPosition.java
@@ -36,4 +36,21 @@ public class FirstMissingPosition {
         nums[i] = nums[j];
         nums[j] = tmp;
     }
+
+    /**
+     * leetcode 287 middle 寻找重复数
+     * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+     * 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+     * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+     */
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+
+        return nums[n - 1];
+    }
 }
